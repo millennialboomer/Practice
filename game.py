c2831cc8d4c1818c49c1514947aa4769e22f7c1a
Kyle <therealmillennialboomer@gmail.com>
2024-02-20 07:14:56 -0600

    Third Commit

--- game.py
+++ game.py
@@ -11,23 +11,24 @@ class Game:
 
         pygame.display.set_caption("Ninja Game")
         self.screen = pygame.display.set_mode((640,480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
-        
+
         self.assets = {
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
         }  
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
-            self.screen.fill((14,219,248))
+            self.display.fill((14,219,248))
 
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -43,7 +44,8 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_d:
                         self.movement[1] = False
-
+                    
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
